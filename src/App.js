efe359a91bbe204f94260af9183028363a955fad
Rustam_9724 <Rustam_9724@mail.ru>
2022-11-14 15:36:21 +0500

    refactor: replacing the <a> attribute with a <Link> attribute

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import { BrowserRouter as Router, Routes, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 
 import { Home } from './pages/Home';
 import { About } from './pages/About';
 import { Contact } from './pages/Contact';
+import { NotFound } from './pages/NotFound'
 
 function App() {
   return (
     <>
-      <Header />
-      <main className="container content">
-        <Router>
-          <Routes>
-            <Route path="/" element={<Home />}/>
-            <Route path="/about" element={<About />}/>
-            <Route path="/contacts" element={<Contact />}/>
-          </Routes>
-        </Router>
-      </main>
-      <Footer />
+      <Router>
+        <Header />
+        <main className="container content">
+            <Routes>
+              <Route path="/" element={<Home />}/>
+              <Route path="/about" element={<About />}/>
+              <Route path="/contacts" element={<Contact />}/>
+              <Route path="*" element={<NotFound />}/>
+            </Routes>
+        </main>
+        <Footer />
+      </Router>
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
+import { Link } from 'react-router-dom';
+
 function Header() {
     return <div>
         <nav className="#bdbdbd grey lighten-1">
             <div className="nav-wrapper">
-                <a href="./" className="brand-logo">React Shop</a>
+                <Link to="/" className="brand-logo">React Shop</Link>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    <li className=""><a href="https://github.com/Rustam9724/react-shop-projects">Repo</a></li>
+                    <li className=""><Link to="/about">About</Link></li>
+                    <li className=""><Link to="/contacts">Contacts</Link></li>
                 </ul>   
             </div>
         </nav>
