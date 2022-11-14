00ad6bf39e33ce95095f939d2e59af5e0e2c5d2a
Rustam_9724 <Rustam_9724@mail.ru>
2022-11-14 15:00:10 +0500

    init: configuration settings and project structures

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+    return <footer className="page-footer #9e9e9e grey">
+    <div className="footer-copyright">
+      <div className="container">
+      © {new Date().getFullYear()} Copyright Text
+      <a className="grey-text text-lighten-4 right" href="https://github.com/Rustam9724/react-shop-project">Repo</a>
+      </div>
+    </div>
+  </footer>
+}
+
+export {Footer};
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+    return <div>
+        <nav className="#bdbdbd grey lighten-1">
+            <div className="nav-wrapper">
+                <a href="./" className="brand-logo">React Shop</a>
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                    <li className=""><a href="https://github.com/Rustam9724/react-shop-projects">Repo</a></li>
+                </ul>   
+            </div>
+        </nav>
+    </div>
+}
+
+export {Header}
