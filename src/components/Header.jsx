71ef8ef007e0fe5e92362abfd939b3df6f8de0b2
Rustam_9724 <Rustam_9724@mail.ru>
2022-11-16 17:31:48 +0500

    init: description on the About page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
     return <div>
         <nav className="#bdbdbd grey lighten-1">
             <div className="nav-wrapper">
-                <Link to="/" className="brand-logo">React Shop</Link>
+                <Link to="/" className="brand-logo">React food</Link>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
                     <li className=""><Link to="/about">About</Link></li>
                     <li className=""><Link to="/contacts">Contacts</Link></li>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,10 @@
 function About() {
-    return <h1>Hello from About page</h1>
+    return (
+        <div className="about-container">
+            <p>React food - educational project built with React. This project is a single page application, with which you can choose a dish to your taste from the presented categories. This application provides all the necessary information about the dish you like: a detailed step-by-step recipe, ingredients and their quantity, and even the country of origin. For those who do not like to read, there is also a video from the best chefs with a step-by-step recipe.</p>
+            <p>Enjoy your meal!</p>
+        </div>
+    )
 }
 
 export { About };
