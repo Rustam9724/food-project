6eb86d3b81bb21d2db63f841e9e04349a67722e8
Rustam_9724 <Rustam_9724@mail.ru>
2022-11-16 17:18:54 +0500

    init: search

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,35 @@
 import { useState, useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { getAllCategories } from '../api';
 import { Preloader } from '../components/Preloader';
 import { CategoryList } from '../components/CategoryList';
+import { Search } from '../components/Search';
 
 function Home() {
     const [catalog, setCatalog] = useState([]);
+    const [filteredCatalog, setFilteredCatalog] = useState([]);
+
+    const { pathname, search } = useLocation();
+
+    const handleSearch = (str) => {
+        setFilteredCatalog(catalog.filter(item => {
+            return item.strCategory.toLowerCase().includes(str.toLowerCase())
+        }))
+    }
 
     useEffect(() => {
         getAllCategories().then(data => {
             setCatalog(data.categories);
+            setFilteredCatalog(data.categories);
         })
     }, [])
 
     return (
         <>
+        <Search cb={handleSearch}/>
         {
             !catalog.length ? <Preloader /> : (
-                <CategoryList catalog={catalog}/>
+                <CategoryList catalog={filteredCatalog}/>
             )
         }
         </>
